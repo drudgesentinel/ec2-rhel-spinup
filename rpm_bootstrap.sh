8ffd41da8c54f2be48f755e2d0e199a4e669f4e8
Sean <sean@seriousbusiness.international>
2024-04-29 16:43:01 -0700

    remove sudo where not needed for cloud-init script

--- rpm_bootstrap.sh
+++ rpm_bootstrap.sh
@@ -1,13 +1,16 @@
 #rpm based distro requirements: https://www.gremlin.com/docs/getting-started-install-virtual-machine#/rpm-packages
+#!/usr/bin/env bash -x
 
+#Send all output of userdata script to /var/log/user-data.log and the console
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 # Install dependencies
-sudo yum install -y iproute-tc
+yum install -y iproute-tc
 
 # Add the Gremlin repo
-sudo curl https://rpm.gremlin.com/gremlin.repo -o /etc/yum.repos.d/gremlin.repo
+curl https://rpm.gremlin.com/gremlin.repo -o /etc/yum.repos.d/gremlin.repo
 
 # Install Gremlin
-sudo yum install -y gremlin gremlind
+yum install -y gremlin gremlind
 
 if [${rhel_version} -eq 8]; then
     yum remove gremlin -y
