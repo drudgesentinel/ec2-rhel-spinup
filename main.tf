4d17b460f83b6495e59381a0c9daae5fb523fb6b
Sean <sean@seriousbusiness.international>
2024-04-29 13:33:44 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "opentofu/aws"
+      version = "~> 5"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+variable "availability_zone" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "os" {
+  type    = string
+  default = "rhel7"
+}
+
+provider "aws" {
+  region = var.availability_zone
+}
