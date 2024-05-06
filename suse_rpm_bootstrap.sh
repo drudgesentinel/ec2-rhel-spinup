3c02771edcde3fd1fefa99578710f94070cbfa4b
Sean <sean@seriousbusiness.international>
2024-05-06 16:57:06 -0700

    add curl to requirements. some SUSE amis don't include it

--- suse_rpm_bootstrap.sh
+++ suse_rpm_bootstrap.sh
@@ -4,7 +4,7 @@
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 # Install dependencies. SLES 15 has iproute-tc2 installed by default
-sudo zypper install -y iproute-tc libcap-progs
+sudo zypper install -y iproute-tc libcap-progs curl
 
 # Add the Gremlin repo
 curl https://rpm.gremlin.com/gremlin.repo -o /tmp/gremlin.repo && sudo zypper addrepo /tmp/gremlin.repo
