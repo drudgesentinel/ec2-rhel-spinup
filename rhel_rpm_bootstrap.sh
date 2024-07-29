c16b085992e8c2cf96b659a7d1e936edb3450d13
Sean <sean@seriousbusiness.international>
2024-07-29 12:10:12 -0700

    try to fix logical or failing during provisioning

--- rhel_rpm_bootstrap.sh
+++ rhel_rpm_bootstrap.sh
@@ -3,17 +3,20 @@
 
 #Send all output of userdata script to /var/log/user-data.log and the console
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+#DBUG
+echo "The rhel version passed to this script was: ${rhel_version}"
+
 # Install dependencies
-yum install -y iproute-tc
+yum install -y iproute-tc vim yum-utils
 
 # Add the Gremlin repo
 curl https://rpm.gremlin.com/gremlin.repo -o /etc/yum.repos.d/gremlin.repo
 
 # Check for RHEL 8 or RHEL 9 and act accordingly
-if [ "${rhel_version}" -eq "8" || "${rhel_version}" -eq "9" ]; then
-    echo "RHEL 8 detected. Installing additional requirements"
-    yum install iproute-tc -y 
-    yum install kernel-modules-extra -y 
+if [ "${rhel_version}" -eq "8" ] || [ "${rhel_version}" -eq "9" ]; then
+    echo "RHEL 8/9 detected. Installing additional requirements"
+    yum install iproute-tc kernel-modules-extra -y 
     yum install kernel-modules-extra-$(uname -r) -y
     modprobe sch_netem
 fi
